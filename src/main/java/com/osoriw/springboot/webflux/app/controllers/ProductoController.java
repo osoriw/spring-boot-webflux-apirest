846f9a6fb34634bf08dd477fc904ed055a221910
waosorio2021 <89051791+waosorio2021@users.noreply.github.com>
2023-08-15 22:08:20 -0500

    Update ProductoController.java
    Creating findById endPoint into ProductoController.

--- src/main/java/com/osoriw/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/osoriw/springboot/webflux/app/controllers/ProductoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,10 +22,17 @@ public class ProductoController {
 	private ProductoService productoService;
 
 	/**
-	 * Combinando Mono y ResponseEntity
+	 * Lista todos los productos (combinando Mono y ResponseEntity)
 	 */
 	@GetMapping
 	public Mono<ResponseEntity<Flux<Producto>>> findAll() {
 		return Mono.just(ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(productoService.findAll()));
 	}
+
+	@GetMapping("/{id}")
+	public Mono<ResponseEntity<Producto>> findById(@PathVariable String id) {
+		return productoService.findById(id)
+				.map(p -> ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(p));
+	}
+
 }
