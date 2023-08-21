e2f433b589c32059c679f10468b915a975050dc1
waosorio2021 <89051791+waosorio2021@users.noreply.github.com>
2023-08-21 10:23:56 -0500

    Creando endpoint para actualizar un producto
    Creando endpoint para actualizar un producto.

--- src/main/java/com/osoriw/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/osoriw/springboot/webflux/app/controllers/ProductoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,4 +57,42 @@ public class ProductoController {
 
 	}
 
+	/**
+	 * Atualizar un producto.
+	 */
+	@PutMapping("/id")
+	public Mono<ResponseEntity<Producto>> update(@RequestBody Producto producto, @PathVariable String id) {
+
+		/*service.findById(id).map(p -> {
+
+		p.setNombre(producto.getNombre());
+		p.setPrecio(producto.getPrecio());
+		p.setCategoria(producto.getCategoria());
+
+		return service.save(p).map(p1 -> ResponseEntity.created(URI.create("/api/productos/".concat(id)))
+				.contentType(MediaType.APPLICATION_JSON).body(p1));
+		});
+
+		return service.findById(id).flatMap(p -> {
+
+		p.setNombre(producto.getNombre());
+		p.setPrecio(producto.getPrecio());
+		p.setCategoria(producto.getCategoria());
+
+		return service.save(p).map(p1 -> ResponseEntity.created(URI.create("/api/productos/".concat(id)))
+				.contentType(MediaType.APPLICATION_JSON).body(p1));
+		});*/
+		
+		return service.findById(id).flatMap(p -> {
+			p.setNombre(producto.getNombre());
+			p.setPrecio(producto.getPrecio());
+			p.setCategoria(producto.getCategoria());
+
+			return service.save(p);
+		}).map(p -> ResponseEntity.created(URI.create("/api/productos/".concat(id)))
+				.contentType(MediaType.APPLICATION_JSON).body(p)).defaultIfEmpty(ResponseEntity.notFound().build());
+
+	}
+	
+	
 }
