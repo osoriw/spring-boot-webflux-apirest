e5d16730fefe44b4f175ef002ab84fc23c9d8a1d
waosorio2021 <89051791+waosorio2021@users.noreply.github.com>
2023-09-10 11:52:36 -0500

    Updating RouterFunctionConfig.java
    Agregando ejemplo de como se puede versionar un mismo endpoint (tener 2 rutas con versiones distintas para un mismo endpoint con la ayuda del operador or).

--- src/main/java/com/osoriw/springboot/webflux/app/RouterFunctionConfig.java
+++ src/main/java/com/osoriw/springboot/webflux/app/RouterFunctionConfig.java
@@ -16,12 +16,15 @@ public class RouterFunctionConfig {
 
 
     /**
-     * Registra las rutas de la API Rest, en el contenedor de Spring.
+     * Este bean permite registrar las rutas de la API Rest, en el contenedor de Spring.
      */
     @Bean
     public RouterFunction<ServerResponse> routes() {
-        return RouterFunctions.route(RequestPredicates.GET("/api/v2/productos"), request -> {
-            return ServerResponse.ok()
+        return RouterFunctions.route(
+                // declarando 2 rutas con versiones diferentes para el mismo endpoint
+                RequestPredicates.GET("/api/v2/productos").or(RequestPredicates.GET("/api/v3/productos")),
+                // implementando el endpoint en el mismo router function
+                request -> { return ServerResponse.ok()
                     .contentType(MediaType.APPLICATION_JSON)
                     .body(service.findAll(), Producto.class);
         });
