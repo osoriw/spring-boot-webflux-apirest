2fb20e6742e7bf19f9f5f44373b331a35b5030d8
waosorio2021 <89051791+waosorio2021@users.noreply.github.com>
2023-09-10 19:27:20 -0500

    Implementando endpoint para actualizar un producto.
    Implementando endpoint para actualizar un producto. Se implementa el método update que hace uso del método zipWith, para mezclar dos flujos reactivos y devolver un sólo flujo con el objeto de que debe ser actualizado en base de datos.

--- src/main/java/com/osoriw/springboot/webflux/app/RouterFunctionConfig.java
+++ src/main/java/com/osoriw/springboot/webflux/app/RouterFunctionConfig.java
@@ -3,6 +3,7 @@ package com.osoriw.springboot.webflux.app;
 import com.osoriw.springboot.webflux.app.handlers.ProductoHandler;
 import com.osoriw.springboot.webflux.app.models.documents.Producto;
 import com.osoriw.springboot.webflux.app.models.services.ProductoService;
+import jdk.jfr.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,7 +26,8 @@ public class RouterFunctionConfig {
                         // declarando 2 rutas con versiones diferentes para el mismo endpoint
                         RequestPredicates.GET("/api/v2/productos").or(RequestPredicates.GET("/api/v3/productos")), handler::findAll)
                 .andRoute(RequestPredicates.GET("/api/v2/productos/{id}"), handler::findById)
-                .andRoute(RequestPredicates.POST("/api/v2/productos"), handler::save);
+                .andRoute(RequestPredicates.POST("/api/v2/productos"), handler::save)
+                .andRoute(RequestPredicates.PUT("/api/v2/productos/{id}"), handler::update);
 
     }
 
--- src/main/java/com/osoriw/springboot/webflux/app/handlers/ProductoHandler.java
+++ src/main/java/com/osoriw/springboot/webflux/app/handlers/ProductoHandler.java
@@ -35,8 +35,8 @@ public class ProductoHandler {
                         .ok()
                         .contentType(MediaType.APPLICATION_JSON)
                         .body(Mono.just(p), Producto.class)) // el producto p, por ser un objeto no reactivo, se debe envolver en un Mono
-                      //.body(BodyInserters.fromValue(p))); // opcional si se quiere usar el método BodyInserters.fromValue(...)
-                        .switchIfEmpty(ServerResponse.notFound().build());
+                //.body(BodyInserters.fromValue(p))); // opcional si se quiere usar el método BodyInserters.fromValue(...)
+                .switchIfEmpty(ServerResponse.notFound().build());
 
     }
 
@@ -45,7 +45,7 @@ public class ProductoHandler {
 
         return productoMono
                 .flatMap(producto -> {
-                    if(producto.getCreateAt() == null){
+                    if (producto.getCreateAt() == null) {
                         producto.setCreateAt(new Date());
                     }
                     return service.save(producto);
@@ -55,9 +55,57 @@ public class ProductoHandler {
                                     .created(URI.create("/api/v2/productos/".concat(producto.getId())))
                                     .contentType(MediaType.APPLICATION_JSON)
                                     .body(BodyInserters.fromValue(producto));
-                                    //.body(Mono.just(producto), Producto.class); // opcional para crear un nuevo mono<Producto>, dentto del body
+                            //.body(Mono.just(producto), Producto.class); // opcional para crear un nuevo mono<Producto>, dentto del body
                         }
                 );
+
+    }
+
+    /**
+     * Actualiza un producto: esta forma de actualizar no es recomendada, sólo se coloca aquí con fines ilustrativos.
+     */
+    /*public Mono<ServerResponse> update(ServerRequest request) {
+        String id = request.pathVariable("id");
+        Mono<Producto> productoMono = request.bodyToMono(Producto.class);
+
+        return productoMono.flatMap(pRequest -> {
+                    return service.findById(id)
+                            .flatMap(pBd -> {
+                                pBd.setNombre(pRequest.getNombre());
+                                pBd.setPrecio(pRequest.getPrecio());
+                                pBd.setCategoria(pRequest.getCategoria());
+
+                                return service.save(pBd);
+                            });
+                })
+                .flatMap(pResponse -> {
+                    return ServerResponse.ok()
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .body(BodyInserters.fromValue(pResponse));
+                });
+
+    }*/
+
+    /**
+     * Actualiza un producto
+     */
+    public Mono<ServerResponse> update(ServerRequest request) {
+        Mono<Producto> pRequest = request.bodyToMono(Producto.class);
+        String id = request.pathVariable("id");
+
+        Mono<Producto> pDb = service.findById(id);
+
+        return pDb.zipWith(pRequest, (db, req) -> {
+                    db.setNombre(req.getNombre());
+                    db.setPrecio(req.getPrecio());
+                    db.setCategoria(req.getCategoria());
+                    return db;
+                })
+                .flatMap(producto -> {
+                    return ServerResponse.created(URI.create("/api/v2/productos/".concat(producto.getId())))
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .body(service.save(producto), Producto.class);
+                });
 
     }
 
