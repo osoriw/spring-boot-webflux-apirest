372706d19b911e120305cd2d694124d2437dc4e4
waosorio2021 <89051791+waosorio2021@users.noreply.github.com>
2023-09-10 20:44:14 -0500

    Agregando nueva condición consumeWith al test unitario findAllProductosTestlist
    Agregando nueva condición consumeWith al test unitario findAllProductosTestlist en SpringBootWebfluxApirestApplicationTests.

--- src/test/java/com/osoriw/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/osoriw/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,11 +1,14 @@
 package com.osoriw.springboot.webflux.app;
 
 import com.osoriw.springboot.webflux.app.models.documents.Producto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
@@ -21,7 +24,12 @@ class SpringBootWebfluxApirestApplicationTests {
                 .exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectBodyList(Producto.class);
+                .expectBodyList(Producto.class)
+                .consumeWith(listEntityExchangeResult -> {
+                    List<Producto> productoList = listEntityExchangeResult.getResponseBody();
+                    productoList.forEach(producto -> System.out.println(producto.getNombre()));
+                });
+
 
     }
 
