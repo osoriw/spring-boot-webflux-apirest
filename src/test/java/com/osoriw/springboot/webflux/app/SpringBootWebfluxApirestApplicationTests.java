0c2807fdcd69d06fcd70d00274361cf795f4f27f
waosorio2021 <89051791+waosorio2021@users.noreply.github.com>
2023-09-10 20:30:01 -0500

    Implementa prueba unitaria del endpoint Get Productos, con WebTestClient
    Implementa prueba unitaria del endpoint Get Productos, con WebTestClient en modo RANDOM_PORT (pruebas contra el ambiente real).

--- src/test/java/com/osoriw/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/osoriw/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,13 +1,28 @@
 package com.osoriw.springboot.webflux.app;
 
+import com.osoriw.springboot.webflux.app.models.documents.Producto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    void testEndpointGetProductos() {
+        client.get()
+                .uri("/api/v2/productos")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Producto.class);
+
+    }
 
 }
